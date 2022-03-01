80d534821d905f7a8326b3283b411cc8b3770d9a
Rikanthan <rikanthanricky@gmail.com>
2022-03-01 11:52:44 +0530

    add class file

---
+++ Assignment1/Application.java
@@ -0,0 +1,7 @@
+public class Application{
+	public static void main(String args[])
+	{
+		B b = new B();
+		b.run();
+	}
+}
---
+++ Assignment1/B.java
@@ -0,0 +1,5 @@
+public class B extends A {
+	void run(){
+		System.out.println("B is running");
+	}					
+}
