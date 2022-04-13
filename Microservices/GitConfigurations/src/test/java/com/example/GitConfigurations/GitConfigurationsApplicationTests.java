08e81b1e0c119ad9da4ed758ee5dc180d8783984
Rikanthan <rikanthanricky@gmail.com>
2022-04-13 11:52:34 +0530

    Add Github configurations

---
+++ Microservices/GitConfigurations/src/test/java/com/example/GitConfigurations/GitConfigurationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GitConfigurations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitConfigurationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
